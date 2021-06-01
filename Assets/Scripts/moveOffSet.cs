570d1e96771a82444a0b7d827e43bc6dc74a60fb
JpBaraky <jp.baraky@gmail.com>
2021-06-01 13:50:29 -0300

    BackGround and Floor
    BG and floor are moving now, created a material for each and a script to move the offset of then

---
+++ Assets/Scripts/moveOffSet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveOffSet : MonoBehaviour
+{
+    private Material material;
+    private playerScript playerScript;
+    public float offSetSpeed;
+    private float offSet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScript = GetComponent<playerScript>();
+        material = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offSet += offSetSpeed * Time.deltaTime;
+        material.SetTextureOffset("_MainTex" , new Vector2(offSet, 0));
+    }
+}
